d93f293ae41bc693981c22e555a99912002ef7f5
Vitor Albano <vitoralbano@gmail.com>
2021-08-23 08:37:06 -0300

    read external file

---
+++ src/br/com/bytebank/banco/test/TesteLeitura.java
@@ -0,0 +1,22 @@
+package br.com.bytebank.banco.test;
+
+import java.io.*;
+import java.nio.CharBuffer;
+
+public class TesteLeitura {
+    public static void main (String[] args) throws IOException {
+        // Fluxo de entrada com arquivo
+        InputStream fis = new FileInputStream("lorem-ipsum.txt");
+        Reader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        var linha = br.readLine();
+
+        while(linha != null) {
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+
+        br.close();
+    }
+}
