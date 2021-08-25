22aba1d2a947b02f460ee2115cb261ee8832dff9
Vitor Albano <vitoralbano@gmail.com>
2021-08-25 09:03:06 -0300

    add scanner for each line

--- src/br/com/bytebank/banco/test/TesteLeituraScanner.java
+++ src/br/com/bytebank/banco/test/TesteLeituraScanner.java
@@ -2,6 +2,7 @@ package br.com.bytebank.banco.test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeituraScanner {
@@ -9,8 +10,29 @@ public class TesteLeituraScanner {
         Scanner scanner = new Scanner(new File("contas.csv"));
 
         while(scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            System.out.println(line);
+            String linha = scanner.nextLine();
+
+            Scanner scannerLinha = new Scanner(linha);
+            scannerLinha.useLocale(Locale.US);
+            scannerLinha.useDelimiter(",");
+
+            String valorTipoConta = scannerLinha.next();
+            int valorAgencia = scannerLinha.nextInt();
+            int valorConta = scannerLinha.nextInt();
+            String valorTitular = scannerLinha.next();
+            double valorSaldo = scannerLinha.nextDouble();
+
+            System.out.println(
+                    String.format(
+                            "Tipo conta: %s, Agência: %d, Conta: %d, Titular: %s, Saldo: %.2f.",
+                            valorTipoConta,
+                            valorAgencia,
+                            valorConta,
+                            valorTitular,
+                            valorSaldo
+                    )
+            );
+            scannerLinha.close();
         }
 
         scanner.close();
