d5309381807901da6e728bbd2d8554da361a2bde
Vitor Albano <vitoralbano@gmail.com>
2021-08-25 08:01:26 -0300

    add new line to the text with System.lineSeparator

--- src/br/com/bytebank/banco/test/TesteEscrita.java
+++ src/br/com/bytebank/banco/test/TesteEscrita.java
@@ -13,7 +13,8 @@ public class TesteEscrita {
         bw.write("industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type");
         bw.newLine();
         bw.write("and scrambled it to make a type specimen book. It has survived not only five centuries, but also");
-        bw.newLine();
+        bw.write(System.lineSeparator());
+        bw.write("the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s");
 
         bw.close();
     }
