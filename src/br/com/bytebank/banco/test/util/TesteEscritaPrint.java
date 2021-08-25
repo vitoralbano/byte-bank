e49b610f38d4a4c3175fd8554b45fda57298a889
Vitor Albano <vitoralbano@gmail.com>
2021-08-25 08:23:49 -0300

    add PrintWriter and PrintStream

---
+++ src/br/com/bytebank/banco/test/util/TesteEscritaPrint.java
@@ -0,0 +1,17 @@
+package br.com.bytebank.banco.test.util;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class TesteEscritaPrint {
+    public static void main (String args[]) throws FileNotFoundException {
+//        PrintStream writer = new PrintStream("teste-escrita.txt");
+        PrintWriter writer = new PrintWriter("teste-escrita.txt");
+
+        writer.println("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the");
+        writer.println();
+        writer.println("industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type");
+        writer.close();
+    }
+}
